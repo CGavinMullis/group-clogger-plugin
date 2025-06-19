9af8b011a8c13b6d7528231d8fd181454852b688
cgavinmullis <cgavinmullis@gmail.com>
2025-06-18 21:33:02 -0400

    feat: example plugin renamed to group clogger

--- src/main/java/com/group/clogger/GroupCloggerPluginConfig.java
+++ src/main/java/com/group/clogger/GroupCloggerPluginConfig.java
@@ -1,11 +1,11 @@
-package com.example;
+package com.group.clogger;
 
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
 
 @ConfigGroup("example")
-public interface ExampleConfig extends Config
+public interface GroupCloggerPluginConfig extends Config
 {
 	@ConfigItem(
 		keyName = "greeting",
--- src/test/java/com/group/clogger/GroupCloggerPluginTest.java
+++ src/test/java/com/group/clogger/GroupCloggerPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.group.clogger;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class GroupCloggerPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(GroupCloggerPlugin.class);
 		RuneLite.main(args);
 	}
 }
